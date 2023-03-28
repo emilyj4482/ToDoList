87a7d27318b68fcecb4b9c2e2e29929be8e0640f
emilyj4482 <emilyj4482@gmail.com>
2023-03-28 22:38:19 +0900

    Cancel button added to AddNewListViewController

--- ToDoList/AddNewListViewController.swift
+++ ToDoList/AddNewListViewController.swift
@@ -16,12 +16,10 @@ class AddNewListViewController: UIViewController {
         // 화면 뜨자마자 텍스트필드 입력 모드(키보드 호출)
         textField.becomeFirstResponder()
     }
-    
-    // 화면 탭할 시 키보드 내림
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
 
+    @IBAction func btnCancelTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
     @IBAction func btnDoneTapped(_ sender: UIButton) {
         dismiss(animated: false)
     }
